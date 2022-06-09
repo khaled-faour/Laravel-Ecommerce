0a94a9438ece224c131b3ea4fa1e83bfcb126d98
Khaled Faour <khaledfaour2@gmail.com>
2022-06-09 18:02:54 +0300

    Added Linking between Categries and Items Models

--- laravel-server/app/Models/Item.php
+++ laravel-server/app/Models/Item.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
 }
