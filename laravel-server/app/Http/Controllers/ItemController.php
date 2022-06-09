d35e8880d6b380c0b8183e583a8bc5f7d273faf1
Khaled Faour <khaledfaour2@gmail.com>
2022-06-09 18:12:01 +0300

    Added Item add Function

--- laravel-server/app/Http/Controllers/ItemController.php
+++ laravel-server/app/Http/Controllers/ItemController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Item;
 
 class ItemController extends Controller
 {
     //
+    public function add(Request $request){
+        $item = new Item;
+
+        $item->name = $request->name;
+        $item->description = $request->description;
+        $item->price = $request->price;
+        $item->save();
+
+        $categories = Category::find($request->categories);
+        $item->categories()->attach($categories);
+    }
 }
