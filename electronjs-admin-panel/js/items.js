4181e169b0a133c3799ac211da7a9b2c6c9e8382
Khaled Faour <khaledfaour2@gmail.com>
2022-06-12 00:09:03 +0300

    Added Item Add Functionality

--- electronjs-admin-panel/js/items.js
+++ electronjs-admin-panel/js/items.js
@@ -1,7 +1,34 @@
+let addModal;
+let addButton;
+let addCloseButton;
+let addSave;
 
 window.addEventListener('load', ()=>{
-    
-    fetchData()
+    addModal = document.getElementById("add-modal");
+    addButton = document.getElementById("add-button");
+    addCloseButton = document.getElementById("add-modal-close");
+    addSave = document.getElementById("add-save");
+
+    addButton.addEventListener('click', ()=>{
+        addModal.style.display = "block";
+    })
+
+    addCloseButton.addEventListener('click', ()=>{
+        addModal.style.display = "none";
+    })
+
+    document.getElementById('add-img').addEventListener('change', (e) => {
+        let reader = new FileReader()
+        reader.readAsDataURL(e.target.files[0]);
+        reader.addEventListener('loadend', () => {
+            console.log(reader.result)
+            document.getElementById('add-img-view').src = reader.result
+        })
+    })
+
+    addSave.addEventListener('click', saveItem)
+    fetchItems()
+    fetchCategories()
 })
 
 const token = localStorage.getItem('token');
@@ -15,10 +42,11 @@ const request = axios.create({
 });
 
 const tableList = document.getElementById('list');
+const categoriesList = document.getElementById('add-categories');
     
 console.log(localStorage.getItem("token"))
 
-async function fetchData(){
+async function fetchItems(){
     await request.get("/store/item").then(response=>{
         tableList.innerHTML = '';
         const items = [...response.data.items];
@@ -38,6 +66,18 @@ async function fetchData(){
         })
     })
 }
+async function fetchCategories(){
+    await request.get("/store/category").then(response=>{
+        console.log(response.data)
+        categoriesList.innerHTML = '';
+        const categories = [...response.data.categories];
+        categories.map(category=>{
+            categoriesList.innerHTML += `
+            <option value="${category.id}">${category.category}</option>
+            `
+        })
+    })
+}
 
 
 function deleteItem(id){
@@ -45,4 +85,27 @@ function deleteItem(id){
 }
 function editItem(id){
     console.log("editing: ", id)
+}
+
+function saveItem(){
+    
+    const name = document.getElementById('add-name')
+    const description = document.getElementById('add-description')
+    const price = document.getElementById('add-price')
+    const categories = document.getElementById('add-categories').selectedOptions
+    const selectedCategories = Array.from(categories).map(option=>option.value)
+    const image = document.getElementById('add-img-view').src;
+
+    if(name.value === "" || description.value === "" || price.value === ""){
+        alert("All fields are required");
+        return
+    }
+    const item = new FormData();
+    item.append('name', name.value)
+    item.append('description', description.value)
+    item.append('price', price.value)
+    item.append('categories', selectedCategories)
+    item.append('image', image)
+
+    request.post('/admin/item', item).then(response=>console.log(response));
 }
