554544c730247e1533a493f4babddd63d06086c4
Khaled Faour <khaledfaour2@gmail.com>
2022-06-11 17:23:30 +0300

    Fixed Few Login Stylings

--- electronjs-admin-panel/js/login.js
+++ electronjs-admin-panel/js/login.js
@@ -7,11 +7,11 @@ const login = async ()=>{
     form.append('password', password);
     if(email === "" || password === "") return;
     await axios.post('http://127.0.0.1:8000/api/login', form).then(response=>{
-        if(response.data.status === "success"){
+        if(response.data.status === "success" && response.data.user.user_type === 1){
             localStorage.setItem("token", response.data.authorisation.token)
         }
     });
-    window.location("./index.html")
+    window.location = "../index.html";
 }
 
 const button = document.getElementById("login-button");
