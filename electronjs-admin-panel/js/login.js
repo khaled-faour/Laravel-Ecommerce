ea6d7a315bf597cd22dab0248f52228dcee1578a
Khaled Faour <khaledfaour2@gmail.com>
2022-06-11 17:13:00 +0300

    Added Login Functionality

---
+++ electronjs-admin-panel/js/login.js
@@ -0,0 +1,18 @@
+
+const login = async ()=>{
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const form = new FormData;
+    form.append('email', email);
+    form.append('password', password);
+    if(email === "" || password === "") return;
+    await axios.post('http://127.0.0.1:8000/api/login', form).then(response=>{
+        if(response.data.status === "success"){
+            localStorage.setItem("token", response.data.authorisation.token)
+        }
+    });
+    window.location("./index.html")
+}
+
+const button = document.getElementById("login-button");
+button.onclick = login;
