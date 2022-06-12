61bcede3734ea5c30a4ebd295d628d9407d6498c
Khaled Faour <khaledfaour2@gmail.com>
2022-06-12 17:30:57 +0300

    Added Get Favorites Functionality

--- front-user/js/home.js
+++ front-user/js/home.js
@@ -1,4 +1,5 @@
 let items = [];
+let favorites = [];
 let itemsList = document.getElementsByClassName('items-list')[0];
 
 let searchInput = document.getElementsByClassName('search')[0];
@@ -10,6 +11,7 @@ window.addEventListener('load', ()=>{
         showItems()
         console.log(e.target.value)
     })
+    fetchFavorites()
     fetchItems()
 })
 
@@ -20,9 +22,18 @@ async function fetchItems(){
     })
 }
 
+async function fetchFavorites(){
+    await axios.get('http://127.0.0.1:8000/api/store/item/favorites', {headers: {Authorization: 'Bearer ' + localStorage.getItem('token')}}).then(response=>{
+        favorites = [...response.data.favorites]
+        console.log(response.data.favorites)
+    })
+}
+
 function showItems(){
     itemsList.innerHTML = ''
     items.filter(item=>item.name.toLowerCase().includes(searchValue.toLowerCase()) || item.description.toLowerCase().includes(searchValue.toLowerCase())).map(item=>{
+        let fav = favorites.find(favorite=>favorite.item_id === item.id)? "solid" : "regular";
+        console.log(fav)
         itemsList.innerHTML += `
         <div class="card">
             <img src='http://127.0.0.1:8000${item.image}'>
@@ -32,7 +43,7 @@ function showItems(){
                     <p>$${item.price.toFixed(2)}</p>
                     <p>${item.description}</p>
                 </div>
-                <span id="like"><i class="fa-regular fa-heart"></i></span>
+                <span id="like"><i class="fa-${fav} fa-heart"></i></span>
             </div>
         </div>
         `
