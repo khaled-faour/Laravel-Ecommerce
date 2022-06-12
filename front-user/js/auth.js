eea036e59a7e423ffa22c42a18ed1cd5f73ef48a
Khaled Faour <khaledfaour2@gmail.com>
2022-06-12 13:11:59 +0300

    Added Auth Redirect Check

--- front-user/js/auth.js
+++ front-user/js/auth.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', ()=>{
 
     const token = localStorage.getItem('token') || null;
-
+    console.log(window.location)
     if(token){
         window.location = './home.html';
-    }else{
-        window.location = 'login.html';
+    }else if(window.location.pathname !== '/login.html'){
+        window.location = './login.html';
     }
 })
